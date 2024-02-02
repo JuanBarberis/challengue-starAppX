5f9c50055059c8a1e2af764525e8a2dd19bb068c
Santi Sarmiento <123501021+JuanBarberis@users.noreply.github.com>
2024-02-02 19:45:50 -0300

    consulta a Api creada

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,11 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { View, Text, StyleSheet } from 'react-native'
 
 const Home = () => {
-    
+
+    const [animals, setAnimals] = useState({})
+    const [loading, setLoading] = useState(true)
+
+    const getAnimals = async () => {
+
+        fetch('https://some-random-api.ml/animal/bird')
+            .then(res => res.json())
+            .then(response => {
+                setAnimals(response)
+                setLoading(false)
+            });
+    }
+
+    useEffect(() => {
+        getAnimals()
+    }, [])
+
+
     return (
         <View style={styles.container}>
-            <Text>Home</Text>
+            {
+                loading === true
+                    ?
+                    <Text>Cargando...</Text>
+                    :
+                    <View>
+                        <Text>Hola</Text>
+                    </View>
+            }
         </View>
     )
 }
@@ -13,9 +39,9 @@ const Home = () => {
 export default Home
 
 const styles = StyleSheet.create({
-    container:{
-        height:'100%',
-        alignItems:'center',
-        justifyContent:'center'
+    container: {
+        height: '100%',
+        alignItems: 'center',
+        justifyContent: 'center'
     }
 })
